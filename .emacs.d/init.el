a257dae25305da7c00c5e6772ab7097084ab7dce
Anand Krishnamoorthi <anakrish@microsoft.com>
2018-12-25 12:07:19 -0800

    Initial experiments.

---
+++ .emacs.d/init.el
@@ -0,0 +1,35 @@
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (zygospore ws-butler w3 volatile-highlights use-package undo-tree solarized-theme rtags iedit helm-swoop helm-projectile helm-gtags dtrt-indent comment-dwim-2 cmake-mode clean-aindent-mode anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'org)
+
+;; Load customizations.
+;; Note: Themest be marked safe (above) before loading so that
+;; there is no prompt each time emacs is run.
+(org-babel-load-file
+ (expand-file-name "emacs.org" user-emacs-directory))
+
+;;
